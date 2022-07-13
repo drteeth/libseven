37e31d7dac34a29f2086e1ce502a2dc5ce09d5ee
Benjamin Moss <drteeth@gmail.com>
2022-07-13 19:13:55 -0400

    fix 32bit arm support

--- build.sh
+++ build.sh
@@ -14,11 +14,11 @@ mkdir -p android/app/src/main/jniLibs/x86
 cargo build --target i686-linux-android --release
 cp `pwd`/target/i686-linux-android/release/libseven.so `pwd`/android/app/src/main/jniLibs/x86/libseven.so
 
-# mkdir -p android/app/src/main/jniLibs/armeabi-v7a
-# cargo build --target armv7-linux-androideabi --release
-# cp `pwd`/target/armv7-linux-androideabi/release/libseven.so `pwd`/android/app/src/main/jniLibs/armeabi-v7a/libseven.so
+mkdir -p android/app/src/main/jniLibs/armeabi-v7a
+cargo build --target armv7-linux-androideabi --release
+cp `pwd`/target/armv7-linux-androideabi/release/libseven.so `pwd`/android/app/src/main/jniLibs/armeabi-v7a/libseven.so
 
 stat `pwd`/android/app/src/main/jniLibs/x86_64/libseven.so
 stat `pwd`/android/app/src/main/jniLibs/arm64-v8a/libseven.so
 stat `pwd`/android/app/src/main/jniLibs/x86/libseven.so
-# stat `pwd`/android/app/src/main/jniLibs/armeabi-v7a/libseven.so
+stat `pwd`/android/app/src/main/jniLibs/armeabi-v7a/libseven.so
--- src/lib.rs
+++ src/lib.rs
@@ -80,7 +80,7 @@ async fn enforce(
     };
 }
 
-// #[cfg(target_os="android")]
+#[cfg(target_os = "android")]
 #[allow(non_snake_case)]
 pub mod android {
     extern crate jni;
